1c6b1133a82931a7827019c52e615015939f8833
kaixin <1875802192@qq.com>
2020-11-22 11:25:53 +0800

    李天龙的网站

---
+++ js/ltljs.js
@@ -0,0 +1,8 @@
+/* 响应式js设置  */
+ function chuang(){  
+    var x=window.innerWidth;    
+    var y=x*100/1898;  
+    document.getElementsByTagName("html")[0].style.fontSize=y+"px";   
+}
+window.onresize=chuang;  
+chuang();
